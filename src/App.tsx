d6ea6c2fe23a502be806703b5695217fe135cc54
mo0nchild <happytown77831@gmail.com>
2024-05-28 00:42:05 +0300

    feat: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Header from './components/Header';
 import './App.css';
-import Catalog from './pages/Catalog';
-import NewComponent from './pages/Test';
-import MainPage from './pages/Main';
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Preview from './pages/Preview';
+import Contacts from './pages/Contacts';
 
 const App: React.FC = () => {
-	const [currentComponent, setCurrentComponent] = useState<number>(0);
-	const [nextComponent, setNextComponent] = useState<number | null>(null);
-	const [isAnimating, setIsAnimating] = useState<boolean>(false);
-	const components = [<MainPage key="main" />, <Catalog key="catalog" />, <NewComponent key="new" />];
-	const containerRef = useRef<HTMLDivElement>(null);
-
-	const animationDuration = 500;
-
-	const handleWheel = (event: WheelEvent) => {
-		if (isAnimating) return;
-
-		if (event.deltaY > 0 && currentComponent < components.length - 1) {
-			setNextComponent(currentComponent + 1);
-			setIsAnimating(true);
-			setTimeout(() => {
-				setCurrentComponent((prev) => prev + 1);
-				setNextComponent(null);
-				setIsAnimating(false);
-			}, animationDuration);
-		} else if (event.deltaY < 0 && currentComponent > 0) {
-			setNextComponent(currentComponent - 1);
-			setIsAnimating(true);
-			setTimeout(() => {
-				setCurrentComponent((prev) => prev - 1);
-				setNextComponent(null);
-				setIsAnimating(false);
-			}, animationDuration);
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Preview/>,
+		},
+		{
+			path: '/contacts',
+			element: <Contacts/>
 		}
-	};
-
-	useEffect(() => {
-		const container = containerRef.current;
-		if (container) {
-			container.addEventListener('wheel', handleWheel);
-		}
-
-		return () => {
-			if (container) {
-				container.removeEventListener('wheel', handleWheel);
-			}
-		};
-	}, [currentComponent, components.length, isAnimating]);
-
-	const getPageStyle = (direction: string): React.CSSProperties => ({
-		animation: `${direction} ${animationDuration}ms forwards`,
-		position: 'absolute',
-		width: '100%',
-		height: '100%',
-		top: 0,
-		left: 0,
-	});
-
-	const currentStyle = getPageStyle(nextComponent !== null && nextComponent > currentComponent ? 'slideOutToTop' : 'slideOutToBottom');
-	const nextStyle = getPageStyle(nextComponent !== null && nextComponent > currentComponent ? 'slideInFromBottom' : 'slideInFromTop');
-
+	]);
 	return (
 		<div className="main-site-content">
 			<Header />
-			<div
-				ref={containerRef}
-				style={{ flexGrow: 1, margin: "52px 0px", height: 'calc(100vh - 82px)', overflow: 'hidden', position: 'relative' }}
-			>
-				<div style={!isAnimating ? {} : currentStyle}>
-					{components[currentComponent]}
-				</div>
-				{isAnimating && nextComponent !== null && (
-					<div style={nextStyle}>
-						{components[nextComponent]}
-					</div>
-				)}
-			</div>
+			<RouterProvider router={router} />
 		</div>
 	);
 };
