05f031a5905a4ca7c939198101c737262c45bb86
mo0nchild <happytown77831@gmail.com>
2024-06-12 17:08:42 +0300

    fix: cors options

--- Factory.Api/src/index.ts
+++ Factory.Api/src/index.ts
@@ -1,15 +1,20 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 import { router } from './controllers/connect';
 
+const corsOptions: cors.CorsOptions = {
+	origin: '*',
+}
 async function main(): Promise<void> {
 	const app = express();
 	const port = process.env.PORT || 3000;
 
+	app.use(cors(corsOptions))
 	app.use(bodyParser.urlencoded())
 	app.use(bodyParser.json())
-	app.use('/bid', router);
 
+	app.use('/bid', router);
 	app.listen(port, () => {
 		console.log(`Server running at http://localhost:${port}`);
 	});
