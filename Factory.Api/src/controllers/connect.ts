5cf68546808cf731fadd7ff62e3164c3fd8db1e9
mo0nchild <happytown77831@gmail.com>
2024-06-12 16:58:56 +0300

    fix: remove html template

--- Factory.Api/src/controllers/connect.ts
+++ Factory.Api/src/controllers/connect.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
-import mailgen from 'mailgen'
 
 import emailSettings from './emailSettings.json'
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
@@ -16,21 +15,8 @@ const transporter = nodemailer.createTransport({
     },
     logger: true
 });
-const mailGenerator = new mailgen({
-    theme: 'default',
-    product: {
-        name: 'Mailgen',
-        link: 'https://mailgen.js/'
-    }
-});
 router.post('/', async (req, res) => {
-    const requestInfo = req.body as BidInfo;
-    const message = mailGenerator.generate({
-        body: {
-            name: 'Заявка на заказ',
-            intro: 'Клиент оставил заявку на заказ',
-        }
-    });
+    const requestInfo = req.body as BidInfo;;
     const info = await transporter.sendMail({
         from: `"Ударник" <${emailSettings.email}>`,
         to: "daniltulenev26@gmail.com",
