b9577fd7bf6e265f24e7528db295545d5d4a23e3
Kovshik387 <fantokin03@mail.ru>
2024-07-26 15:21:06 +0300

    новый положняк: адаптив

--- Factory.Web/src/components/Header.tsx
+++ Factory.Web/src/components/Header.tsx
@@ -134,7 +134,7 @@ export const Header = React.forwardRef<HeaderHandler, {}>((_, ref) => {
 const imageStyle: React.CSSProperties = {
     width: '250px',
     height: '86px',
-    // paddingRight: "45px"
+    paddingRight: "65px"
 };
 
 const headerStyle: React.CSSProperties = {
